0f1834679e065914cd0f47744941a13b9f3720f3
Keith Estes <keithestes@paiconsulting.com>
2025-11-25 16:17:02 -0500

    Set Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  return {
+    base: '/pai-cictt-hazard-identifier/',   // <-- ADD THIS LINE
+
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+
+    plugins: [react()],
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
